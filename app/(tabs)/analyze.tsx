7f8eb6cc99f56ee454e018bf3a19922a56376be8
MohiAssaf <mohamedabuassaf@gmail.com>
2025-05-05 18:51:02 +0300

    init project structure

--- app/(tabs)/analyze.tsx
+++ app/(tabs)/analyze.tsx
@@ -1,9 +1,9 @@
 import { SafeAreaView, Text } from "react-native";
 
-export default function HomeScreen() {
+export default function TabTwoScreen() {
   return (
     <SafeAreaView className="flex-1 justify-center items-center w-full h-full">
-      <Text className="text-2xl">Home screen</Text>
+      <Text className="text-2xl">Analyze screen</Text>
     </SafeAreaView>
   );
 }
