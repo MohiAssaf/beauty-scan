716c5497c628110f037e91dc3efea6addf338c4b
MohiAssaf <mohamedabuassaf@gmail.com>
2025-05-24 22:25:16 +0300

    adding a const file for the colors that will be through out the app for better ui styling

---
+++ constants/colors.ts
@@ -0,0 +1,19 @@
+export const COLORS = {
+  primary: {
+    from: "#2563EB",
+    to: "#1E40AF",
+  },
+  text: {
+    dark: "#1E293B",
+    medium: "#334155",
+    light: "#64748B",
+  },
+  background: {
+    light: "#F8FAFC",
+    white: "#FFFFFF",
+  },
+  accent: {
+    purple: "#7C3AED",
+    green: "#059669",
+  },
+};
